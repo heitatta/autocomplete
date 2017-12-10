88f5a484b90c3d0922dc21e94c0df4a253632b43
Tadashi Hirata <heita@nifty.com>
2017-12-10 12:42:47 +0000

    add data generation code (dataflow)

---
+++ dataflow/bestbuy2bq.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+suppose big query talbe exists by:
+  bq mk bestbuy
+"""
+
+import apache_beam as beam
+import datetime
+import time
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions
+
+# --requirements_file requirements.txt
+
+# Create and set your PipelineOptions.
+options = PipelineOptions(flags=['--requirements_file=requirements.txt'])
+
+# For Cloud execution, set the Cloud Platform project, job_name,
+# staging location, temp_location and specify DataflowRunner.
+google_cloud_options = options.view_as(GoogleCloudOptions)
+google_cloud_options.project = 'sample-datalab'
+google_cloud_options.job_name = ("autocomplete-raw-%s" % datetime.datetime.today().strftime("%m%dt%H%M"))
+google_cloud_options.staging_location = 'gs://heita-datalab-dataflow/binaries'
+google_cloud_options.temp_location = 'gs://heita-datalab-dataflow/temp'
+options.view_as(StandardOptions).runner = 'DataflowRunner'
+
+
+def fetch_data(api_key):
+  import requests
+  page_size = 100
+  cursor_mark = "*"
+  while(True):
+    for i in range(0,10):
+      res = requests.get("https://api.bestbuy.com/v1/products?format=json&" +\
+                         "cursorMark=%s&show=name,releaseDate,bestSellingRank&pageSize=%d&apiKey=%s"
+                         % (cursor_mark, page_size, api_key))
+      if res.status_code == 200:
+          break
+      time.sleep(1)
+    products = res.json()
+    if 'products' not in products or len(products['products']) == 0:
+        break
+    for product in products['products']:
+        if product['bestSellingRank'] is None:
+            product['bestSellingRank'] = 0
+        yield(product)
+    cursor_mark = products['nextCursorMark']
+
+def id_func(arg):
+  return arg
+
+
+p = beam.Pipeline(options=options)
+(p | 'api key'         >> beam.Create(['pHRhgOkDJ8cmKCmKKoYwPDH3'])
+   | 'fetch data'      >> beam.FlatMap(fetch_data)
+   | 'id dummy'        >> beam.Map(id_func)
+   | 'write to bq'     >> beam.io.Write(beam.io.BigQuerySink('bestbuy.products2',
+                              schema="name:STRING, releaseDate:DATE, bestSellingRank:INTEGER",
+                              write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                              create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED))
+)
+p.run() #.wait_until_finish()
---
+++ dataflow/bq2ds.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import uuid
+import datetime
+import re
+from copy import deepcopy
+
+import apache_beam as beam
+from googledatastore import helper
+from google.cloud.proto.datastore.v1 import entity_pb2
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions
+from apache_beam.io.gcp.datastore.v1.datastoreio import WriteToDatastore
+
+options = PipelineOptions(flags=['--save_main_session',
+                                 '--requirements_file=requirements.txt'])
+google_cloud_options = options.view_as(GoogleCloudOptions)
+google_cloud_options.project = 'sample-datalab'
+google_cloud_options.job_name = ("autocomplete-genkey-%s" % datetime.datetime.today().strftime("%m%dt%H%M"))
+google_cloud_options.staging_location = 'gs://heita-datalab-dataflow/binaries'
+google_cloud_options.temp_location = 'gs://heita-datalab-dataflow/temp'
+options.view_as(StandardOptions).runner = 'DataflowRunner'
+  
+def new_elm(element, key):
+    elm = deepcopy(element)
+    elm['key'] = key
+    return elm
+
+def gen_key(element):
+  # just make it uppercase is a key
+  org_key = element['name'].upper()
+  result = [new_elm(element, org_key)]
+  # remove one words each and make them keys
+  splitted_name = re.split('\s+', org_key)
+  for i in range(1, len(splitted_name)):
+    splitted_name[i] = re.sub('\W', '', splitted_name[i])
+    result.append(new_elm(element, " ".join(splitted_name[i:])))
+  return result
+  
+class EntityWrapper(object):
+  def __init__(self, namespace, kind):
+    self._namespace = namespace
+    self._kind = kind
+
+  def make_entity(self, content):
+    entity = entity_pb2.Entity()
+    if self._namespace is not None:
+      entity.key.partition_id.namespace_id = self._namespace
+
+    helper.add_key_path(entity.key, self._kind, str(uuid.uuid4()))
+    helper.add_properties(entity, content)
+    return entity
+
+with beam.Pipeline(options=options) as p:
+  (p | 'query from bq'       >> beam.io.Read(beam.io.BigQuerySource(query="select * from bestbuy.products"))
+     | 'generate key'        >> beam.FlatMap(gen_key)
+     | 'make entry'          >> beam.Map(EntityWrapper(None, 'products').make_entity)
+     | WriteToDatastore("sample-datalab")
+  )
